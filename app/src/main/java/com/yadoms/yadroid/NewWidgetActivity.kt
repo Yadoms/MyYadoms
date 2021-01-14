1c241a561533110d4379e807faf8a4d6f4612268
Sébastien Gallou <sebastien.gallou@gmail.com>
2021-01-14 18:18:15 +0100

    Add action bar back support on addNewWidget activity

--- app/src/main/java/com/yadoms/yadroid/NewWidgetActivity.kt
+++ app/src/main/java/com/yadoms/yadroid/NewWidgetActivity.kt
@@ -1,8 +1,11 @@
 package com.yadoms.yadroid
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.yadoms.yadroid.databinding.ActivityNewWidgetBinding
 import com.yadoms.yadroid.yadomsApi.DeviceApi
@@ -15,6 +18,7 @@ class NewWidgetActivity : AppCompatActivity() {
     var selectedWidgetType: WidgetTypes.WidgetTypeItem? = null
 
     private lateinit var binding: ActivityNewWidgetBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,13 +26,30 @@ class NewWidgetActivity : AppCompatActivity() {
         binding = ActivityNewWidgetBinding.inflate(layoutInflater)
         val view = binding.root
 
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val navController = navHostFragment.navController
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.navController
 
         setContentView(view)
 
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title = "Add new widget"
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        return when (item.itemId) {
+            android.R.id.home -> {
+                if (!navController.navigateUp()) {
+                    val intent = Intent(this@NewWidgetActivity, ScrollingActivity::class.java)
+                    startActivity(intent)
+                }
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 
     fun setOperationDescription(textId: Int) {
