230addc4fd2ea1be6080bb67a9a11cced9e4410b
Sébastien Gallou <sebastien.gallou@gmail.com>
2021-01-26 08:11:37 +0100

    Add undo

--- app/src/main/java/com/yadoms/yadroid/WidgetsRecyclerViewAdapter.kt
+++ app/src/main/java/com/yadoms/yadroid/WidgetsRecyclerViewAdapter.kt
@@ -3,19 +3,28 @@ package com.yadoms.yadroid
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.yadoms.yadroid.preferences.Preferences
 import com.yadoms.yadroid.widgets.WidgetViewHolder
 
-//TODO bannir les !!
+
+//TODO bannir les !! (partout)
+//TODO mettre firebase
 
 class WidgetsRecyclerViewAdapter(val preferences: Preferences) : RecyclerView.Adapter<WidgetViewHolder>() {
     private var widgets = preferences.widgets as MutableList
+
+    private lateinit var view: ViewGroup
+
+    private lateinit var recentlyDeletedWidget: Preferences.Widget
+    private var recentlyDeletedWidgetPosition = 0
 
     override fun getItemViewType(position: Int): Int {
         return widgets[position].type.ordinal
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WidgetViewHolder {
+        view = parent
         val widgetTypeItem = WidgetTypes.item(WidgetTypes.WidgetType.values()[viewType])!!
 
         val view = LayoutInflater.from(parent.context)
@@ -37,8 +46,30 @@ class WidgetsRecyclerViewAdapter(val preferences: Preferences) : RecyclerView.Ad
     }
 
     fun deleteWidget(position: Int) {
+        recentlyDeletedWidget = widgets.get(position);
+        recentlyDeletedWidgetPosition = position;
+
         widgets.removeAt(position)
         preferences.saveWidgets(widgets)
         notifyItemRemoved(position)
+        showUndoSnackbar();
+    }
+
+    private fun showUndoSnackbar() {
+        val snackbar: Snackbar = Snackbar.make(
+            view, view.context.getString(com.yadoms.yadroid.R.string.widget_deleted),
+            Snackbar.LENGTH_LONG
+        )
+        snackbar.setAction(view.context.getString(com.yadoms.yadroid.R.string.undo)) { undoDelete() }
+        snackbar.show()
+    }
+
+    private fun undoDelete() {
+        widgets.add(
+            recentlyDeletedWidgetPosition,
+            recentlyDeletedWidget
+        )
+        preferences.saveWidgets(widgets)
+        notifyItemInserted(recentlyDeletedWidgetPosition)
     }
 }
