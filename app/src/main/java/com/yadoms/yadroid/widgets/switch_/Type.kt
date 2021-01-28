df279d780fe11dd8c5f40bc48b9aa2f62def2d15
Sébastien Gallou <sebastien.gallou@gmail.com>
2021-01-28 17:26:46 +0100

    Add GetSet access filter on switch widget

--- app/src/main/java/com/yadoms/yadroid/widgets/switch_/Type.kt
+++ app/src/main/java/com/yadoms/yadroid/widgets/switch_/Type.kt
@@ -5,11 +5,14 @@ import com.yadoms.yadroid.widgets.WidgetTypes
 import com.yadoms.yadroid.yadomsApi.DeviceApi
 
 val type = WidgetTypes.WidgetTypeItem(
-        type = WidgetTypes.WidgetType.Switch,
-        nameRessourceId = R.string.switch_name,
-        descriptionRessourceId = R.string.switch_description,
-        logo = R.drawable.ic_switch1,
-        keywordFilter = WidgetTypes.KeywordFilter(expectedKeywordType = arrayOf(DeviceApi.KeywordTypes.Bool)),
-        layout = R.layout.widget_switch_item,
-        createViewHolder = { ViewHolder(it) }
+    type = WidgetTypes.WidgetType.Switch,
+    nameRessourceId = R.string.switch_name,
+    descriptionRessourceId = R.string.switch_description,
+    logo = R.drawable.ic_switch1,
+    keywordFilter = WidgetTypes.KeywordFilter(
+        expectedKeywordType = arrayOf(DeviceApi.KeywordTypes.Bool),
+        expectedKeywordAccess = arrayOf(DeviceApi.KeywordAccess.GetSet)
+    ),
+    layout = R.layout.widget_switch_item,
+    createViewHolder = { ViewHolder(it) }
 )
